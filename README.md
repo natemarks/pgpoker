70b4795a0fdcfe8be3b77f882d32966c4584e11d
Nate Marks <nmarks@imprivata.com>
2023-11-11 07:21:06 -0500

    nit

---
+++ README.md
@@ -0,0 +1,16 @@
+# looch
+this project is an apprunner/cdk  experiment. It includes a go program that
+does something on an interval  and logs/alerts teh results
+
+
+
+Goals
+
+ - apprunner runs a docker image deployed
+ - the image contains an app that runs  some curl command on an interval
+ - the results are logged to cloudwatch
+ - problens are alerted to zendesk
+ - the app is deployed with CDK
+
+
+Features
---
+++ cmd/looch/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/rs/zerolog"
+	"os"
+	"strconv"
+	"time"
+)
+
+func getInterval() int {
+	intervalStr, exists := os.LookupEnv("INTERVAL")
+	if !exists {
+		panic("Environment variable 'INTERVAL' not set")
+	}
+
+	interval, err := strconv.Atoi(intervalStr)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to convert 'interval' to an integer: %v", err))
+	}
+
+	return interval
+}
+func main() {
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
+	interval := getInterval()
+	logger.Info().Msgf("starting with interval: %v", interval)
+	for {
+		logger.Info().Msgf("sleeping for interval: %v", interval)
+		time.Sleep(time.Duration(interval) * time.Second)
+	}
+}
